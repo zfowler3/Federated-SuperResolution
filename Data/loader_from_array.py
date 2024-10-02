67f9cc0206ac98bd99fd2796d86f52b59a46e6f0
zoe <zfowler3@gatech.edu>
2024-10-02 16:31:13 -0400

    testing out gradient stuff - seeing if this is valid

---
+++ Data/loader_from_array.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+from PIL import Image
+from torch.utils.data import Dataset
+import numpy as np
+from medmnist import PneumoniaMNIST
+from torchvision import transforms
+
+
+class MedMNIST_Testing(Dataset):
+    """MedMNIST dataset class"""
+
+    def __init__(self, input_arr):
+        """Initialize MedMNISTDataset."""
+
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[.5], std=[.5])
+        ])
+
+        labels = np.load('/media/zoe/ssd/pneumoniamnist_224/test_labels.npy')
+
+        self.x, self.targets = input_arr, labels.squeeze()
+
+    def __getitem__(self, index: int):
+        """Return an item by the index."""
+        img = self.x[index]
+
+        img = Image.fromarray(img)
+
+        if self.transform:
+            img = self.transform(img)
+
+        y = self.targets[index]
+
+        return img, y
+
+    def __len__(self) -> int:
+        """Return the len of the dataset."""
+        return len(self.x)
