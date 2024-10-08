ff7148f3753431518ef2629f9df5cd08941c7534
zoe <zfowler3@gatech.edu>
2024-10-08 09:57:49 -0400

    debugging seismic for unet++

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 
 from Data.dataloader import InlineLoader
-from Train.train_one_epoch import train_epoch
+from Train.train_one_epoch import train_epoch, eval_epoch
 from Utils.get_seismic_data import get_dataset_seismic
 
 
@@ -87,7 +87,12 @@ def main():
     model = model.to(device)
     criterion = nn.CrossEntropyLoss().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    user = user_groups[0]
     for e in range(args.local_ep):
-        user = user_groups[0]
-        train_epoch(data_loader=user["train"], model=model, criterion=criterion, optimizer=optimizer, device=device,
-                    epoch=e, dataset='seismic', model_type='unet')
+        loss, model = train_epoch(data_loader=user["train"], model=model, criterion=criterion, optimizer=optimizer,
+                                  device=device, epoch=e, dataset='seismic', model_type='unet')
+        print('Epoch {}/{}: Loss {}'.format(e, args.local_ep, loss))
+    print('Testing')
+    test_loss, preds = eval_epoch(data_loader=test_loader, model=model, criterion=criterion, device=device,
+                                  save_file=test_labels)
+
