26d38e869660ae9d0107cc00885926190d634c7f
zoe <zfowler3@gatech.edu>
2024-10-14 14:47:43 -0400

    weighted avg done

--- Utils/aggregation.py
+++ Utils/aggregation.py
@@ -12,7 +12,7 @@ def aggregate(preds, psnr, mode='majority'):
         elif mode == 'avg':
             final_pred = average_vote(preds=preds, img_num=i)
         else:
-            final_pred = weighted_vote(preds=preds, img_num=i)
+            final_pred = weighted_vote(preds=preds, psnr=psnr, img_num=i)
 
         overall[:, i, :] = final_pred
 
@@ -36,11 +36,21 @@ def average_vote(preds, img_num):
     for c in range(len(preds)):
         combined[:, c, :] = preds[c]["pred"][:, img_num, :]
 
-    # For specific image, do pixel-wise average (and round)
+    # For specific image, do pixel-wise average (and round to give integer vals)
     avg_pred = np.round(np.mean(combined, axis=0))
     return avg_pred
 
-def weighted_vote(preds, img_num):
+def weighted_vote(preds, to_weight, img_num):
+    wts_for_img = []
+    for c in range(len(preds)):
+        wts_for_img.append(to_weight[c]["psnr"][img_num])
+    weights = np.array(wts_for_img) / np.sum(wts_for_img)
 
-
-    return
+    s = preds[0]["pred"][:, img_num, :]
+    x, y = s.shape[0], s.shape[1]
+    combined = np.zeros(shape=(len(preds), x, y))
+    for c in range(len(preds)):
+        combined[:, c, :] = preds[c]["pred"][:, img_num, :]
+    # Do weighted average, where each image is weighted wrt its produced score
+    weighted_pred = np.round(np.average(combined, axis=0, weights=weights))
+    return weighted_pred
